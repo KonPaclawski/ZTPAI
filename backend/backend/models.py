eeda285b771758bb3904faf5cb06829d172dc630
Koniett <kpac987@gmail.com>
2025-04-01 13:56:27 +0200

    feat(model): added payment, changed Budget

--- backend/backend/models.py
+++ backend/backend/models.py
@@ -1,14 +1,20 @@
 from django.db import models
 
 class User(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=255) 
-    created_at = models.DateTimeField(auto_now_add=True)
 
 class Budget(models.Model):
+    id = models.AutoField(primary_key=True)
+    id_user = models.ForeignKey('User', on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     category = models.CharField(max_length=255)
+    
+class Payment(models.Model):
+    id_budget = models.ForeignKey('Budget', on_delete=models.CASCADE)
     payment_title = models.CharField(max_length=255)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
-    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    date = models.DateField()
+    
